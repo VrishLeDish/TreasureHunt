639c8b438f8ab347502f4efa7446e16bd5dda8a6
VrishLeDish <124202638+VrishLeDish@users.noreply.github.com>
2023-09-17 18:44:22 +0100

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "RITU MAAM";
+const accessCode2 = "VRISHAN";
+const accessCode3 = "JAVASCRIPT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T U R I A M A M", 100,50)
+    fill("lightblue")
+    text("Hint: Best Coding Teacher!!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("S A H N R I V", 700,150)
+    fill("lightblue")
+    text("Hint: No. 1 Coder ", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("S I P C R J V A T A", 100,250)
+    fill("lightblue")
+    text("Hint: A Programming Language", 100,270)
+
+}
